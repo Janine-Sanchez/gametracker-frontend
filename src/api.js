7f14b73ff125e76c510cf45db74b3cb026628f39
Maria-Alq <maria.alquinga@epn.edu.ec>
2025-11-15 00:05:27 -0500

    Eliminacion de componentes y clases que no se usan

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Configuración de la instancia de axios
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',  // Cambia esto si tu backend está en otra URL
+  baseURL: 'http://localhost:5000/api',  
 });
 
 export { api };
